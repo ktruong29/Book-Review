d0feb2ac52c1c24461999b51864118a0e0d97eaf
Shelly-ss <72415765+Shelly-ss@users.noreply.github.com>
2021-02-18 15:48:44 -0800

    Update pkgs.sh

--- pkgs.sh
+++ pkgs.sh
@@ -3,8 +3,8 @@
 sudo apt-get update -y
 sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
-sudo pip3 install pymysql #doesn't work for me, sudo: pip3: command not found
-sudo pip3 install mysql-connector
+sudo pip3 install pymysql             #sudo apt install python3-pip
+sudo pip3 install mysql-connector     #sudo apt-get install python3-mysql.connector
 sudo pip3 install Flask
 sudo pip3 install Flask-WTF
 sudo pip3 install passlib
