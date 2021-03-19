a214fb0012e1bd937a7a86b457345e0cf65015bd
Kien Truong <ktruong29@csu.fullerton.edu>
2021-03-19 12:12:25 -0700

    Added Shelly's db address

--- app.py
+++ app.py
@@ -18,9 +18,11 @@ app = Flask(__name__)
 app.secret_key = 'hjkdjeuqoe157!@'
 
 #customhost = <your db endpoint address>
-DB_HOST      = "database-1.cykqbf99bmvw.us-west-1.rds.amazonaws.com"
+DB_HOST      = "cpsc362.cqz5lsbthplh.us-east-2.rds.amazonaws.com"
+# DB_HOST      = "database-1.cykqbf99bmvw.us-west-1.rds.amazonaws.com"
 DB_USER      = "admin"
-DB_PASS      = "group362"
+# DB_PASS      = "group362"
+DB_PASS      = "adminadmin"
 DB_NAME      = "LIBRARY"
 
 #Connecting to MySQL instance RDS
