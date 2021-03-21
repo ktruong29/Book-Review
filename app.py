37f154d20a6ee0443dd3783275fa1fd8a4b045e1
Kien Truong <ktruong29@csu.fullerton.edu>
2021-03-20 18:33:39 -0700

    Added some comments + change picture functionality

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, redirect, request, session, flash
 import pymysql
-from wtforms import Form, TextField, PasswordField, validators
+from wtforms import Form, TextField, SubmitField, PasswordField, validators
 from wtforms.fields.html5 import EmailField
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from werkzeug.utils import secure_filename
 from passlib.hash import sha256_crypt
 from functools import wraps
 import config
@@ -11,10 +14,13 @@ import gc
 import sys
 import logging
 import os
+# from flask_wtf.csrf import CsrfProtect
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
+# CsrfProtect(app)
 app.secret_key = 'hjkdjeuqoe157!@'
+
 
 #customhost = <your db endpoint address>
 DB_HOST      = "cpsc362.cqz5lsbthplh.us-east-2.rds.amazonaws.com"
@@ -24,6 +30,8 @@ DB_USER      = "admin"
 DB_PASS      = "adminadmin"
 DB_NAME      = "LIBRARY"
 
+#Path to save profile pictures
+DIR_PATH = "/home/kient/project362/cpsc362-group-project/static/images"
 #Connecting to MySQL instance RDS
 try:
     db = pymysql.connect(host=DB_HOST, port=3306, user=DB_USER, password=DB_PASS, db=DB_NAME)
@@ -48,6 +56,11 @@ class ChangePasswordForm(Form):
     new_password = PasswordField('New Password', [validators.DataRequired(), validators.Length(min=6, max=20),
                                           validators.EqualTo('confirm', message="Password must match")])
     confirm  = PasswordField('Repeat New Password')
+
+class PhotoForm(FlaskForm):
+    new_photo = FileField('Upload a new picture', validators=[FileRequired(),
+                                  FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def home():
@@ -99,10 +112,8 @@ def login_required(f):
 @app.route('/login', methods=["GET","POST"])
 def login_page():
     error = ''
-    # app.logger.info('Yes7~~')
     try:
         if request.method == 'POST':
-            # app.logger.info('Yes6~~')
             cursor = db.cursor()
             username = request.form['username']
             data = cursor.execute("SELECT * FROM PERSON WHERE Username = (%s)", (username))
@@ -110,16 +121,15 @@ def login_page():
             cursor.close()
 
             if sha256_crypt.verify(request.form['password'], data[6]):
-                session['logged_in'] = True
-                session['username'] = username
-                session['first_name'] = data[1]
-                session['pic_name'] = data[7]
+                session['logged_in']    = True
+                session['username']     = username
+                session['first_name']   = data[1]
+                session['pic_name']     = data[7]
                 flash("You are now logged in")
                 return redirect('/dashboard')
             else:
                 error = "Invalid credentials, try again."
         gc.collect()
-        # app.logger.info('Yes8~~')
         return render_template("login.html", error=error)
 
     except Exception as e:
@@ -129,9 +139,19 @@ def login_page():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dahboard():
-    # username = session['username']
-    first_name = session['first_name']
-    pic_name   = session['pic_name']
+    username    = session['username']
+    first_name  = session['first_name']
+    ''' ***********************************************************************
+    --- This will update the latest uploaded image to appear on the dashboad page,
+    --- which occurs at @app.route('/change_pic') ---
+    *************************************************************************'''
+    cursor  = db.cursor()
+    data    = cursor.execute("SELECT * FROM PERSON WHERE Username = (%s)", (username))
+    data    = cursor.fetchone()
+    cursor.close()
+    session['pic_name'] = data[7]
+    pic_name = session['pic_name']
+    gc.collect()
     return render_template('dashboard.html', first_name=first_name, pic_name=pic_name)
 
 @app.route('/dashboard/change_passwd', methods=['GET', 'POST'])
@@ -139,46 +159,56 @@ def dahboard():
 def change_passwd():
     error = ''
     username = session['username']
+    pic_name = session['pic_name']
     try:
         form = ChangePasswordForm(request.form)
         if request.method == 'POST' and form.validate():
+            ''' ***************************************************************
+            --- Extracting fields filled out by user in the change password form
+            *****************************************************************'''
             oldpassword = form.old_password.data
             newpassword = form.new_password.data
-            confirm = form.confirm.data
+            confirm     = form.confirm.data
 
-            # app.logger.info('Yes6~~')
-            cursor = db.cursor()
-            data = cursor.execute("SELECT * FROM PERSON WHERE Username = (%s)", (username))
-            data = cursor.fetchone()
+            cursor  = db.cursor()
+            data    = cursor.execute("SELECT * FROM PERSON WHERE Username = (%s)", (username))
+            data    = cursor.fetchone()
             cursor.close()
 
+            ''' ***************************************************************
+            --- Validating if the new passwords are entered correctly
+            ---     If it's not, then the program will display an error on the web
+            ---     page and ask for another try.
+            ---     If they are equal, then the program will compare the hash values
+            ---     between the old_password (entered by user) and the current password
+            ---     stored in the db. If they are not the same, an error will be
+            ---     displayed and user will have another attempt. If they are
+            ---     equal, then the program will update the new password to db.
+            *****************************************************************'''
             if newpassword != confirm:
                 error = "New password must match"
             else:
                 if sha256_crypt.verify(oldpassword, data[6]):
                     new_password = sha256_crypt.encrypt((str(newpassword)))
-
                     # update the new password to the database
                     cursor = db.cursor()
                     data = cursor.execute("UPDATE PERSON SET Password = (%s) WHERE Username = (%s)", (new_password, username))
-                    data = cursor.fetchone()
+                    # data = cursor.fetchone()
                     db.commit()
                     cursor.close()
-
                     flash("You have changed your password!")
                     return redirect('/dashboard')
                 else:
                     error = "Invalid old password, try again."
-
         else:
             error = "Please fill in all of the field in the form"
 
         gc.collect()
-        return render_template('change_passwd.html', error=error, first_name=username, form=form)
+        return render_template('change_passwd.html', error=error, first_name=username, form=form, pic_name=pic_name)
     except Exception as e:
         error = str(e)
 
-    return render_template('change_passwd.html', error=error, form=form)
+        return render_template('change_passwd.html', error=error, form=form)
 
 @app.route('/logout')
 @login_required
@@ -187,6 +217,41 @@ def logout():
     gc.collect()
     flash("You have been logged out")
     return redirect('/')
+
+@app.route('/dashboard/change_pic', methods=['GET', 'POST'])
+@login_required
+def change_pic():
+    error = ''
+    username   = session['username']
+    first_name = session['first_name']
+    pic_name   = session['pic_name']
+    try:
+        form = PhotoForm()
+        if form.validate_on_submit():
+            pic_name = form.new_photo.data
+            filename = secure_filename(pic_name.filename)
+            ''' ****************************************************************
+            ---------- Save the newly updated picture locally ----------
+            *****************************************************************'''
+            pic_name.save(os.path.join(DIR_PATH, pic_name.filename))
+
+            ''' ****************************************************************
+            ---------- Update Profile_URL (filename of pix) to db ----------
+            ******************************************************************'''
+            cursor  = db.cursor()
+            data    = cursor.execute("UPDATE PERSON SET Profile_URL = (%s) WHERE Username = (%s)",
+                                    (pic_name.filename, username))
+            db.commit()
+            cursor.close()
+            gc.collect()
+            flash("You have successfully updated your profile picture")
+            return redirect('/dashboard')
+        # gc.collect()
+        app.logger.info('Yes14~~')
+        return render_template('change_pic.html', pic_name=pic_name, first_name=first_name, error=error, form=form)
+    except Exception as e:
+        error = str(e)
+        return render_template('change_pic.html', pic_name=pic_name, first_name=first_name, error=error, form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
