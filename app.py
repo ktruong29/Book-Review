d7422b47bd59ce0e799f735c9d95acbafd7e6a64
Shelly-ss <72415765+Shelly-ss@users.noreply.github.com>
2021-04-28 20:50:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -157,10 +157,6 @@ def dahboard():
     books    = cursor.fetchall()
     cursor.close()
 
-    if request.method == "POST":
-        search = cursor.execute("SELECT * FROM BOOK WHERE Title = (%s)", request.form['search'])
-        return render_template("results.html", records=cursor.fetchall())
-
     gc.collect()
     return render_template('dashboard.html', first_name=first_name, pic_name=pic_name, books=books)
 
@@ -170,7 +166,7 @@ def search_result():
     if request.method == "POST" and len(request.form['search']) > 0:
         search = request.form['search']
         cursor  = db.cursor()
-        search_result    = cursor.execute("SELECT * FROM BOOK WHERE Title LIKE %s", ('%' + search + '%'))
+        search_result    = cursor.execute("SELECT * FROM BOOK WHERE Title or AuthorName LIKE %s", ('%' + search + '%'))
         search_result    = cursor.fetchall()
         cursor.close()
         gc.collect()
